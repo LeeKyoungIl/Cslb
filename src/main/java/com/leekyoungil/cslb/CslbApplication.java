dee383305cb182301db96d9b3874bcb63c12beb4
Kellin.lee <kellin.lee@kakaocorp.com>
2016-09-16 23:08:47 +0900

    초기 셋팅

---
+++ src/main/java/com/leekyoungil/cslb/CslbApplication.java
@@ -0,0 +1,12 @@
+package com.leekyoungil.cslb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CslbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CslbApplication.class, args);
+	}
+}
